8915e6f42a7b37a2ba42e99ed5a8171a43100c55
Jonas Gustavsson <jonasgson@outlook.com>
2020-08-12 13:49:14 +0200

    Updates

---
+++ component---src-pages-404-jsx-2aa192112b95c5c07fda.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{pssB:function(t,e,n){"use strict";n.r(e),n.d(e,"pageQuery",(function(){return l}));var a=n("q1tI"),u=n.n(a),o=n("ntAx"),r=n("dmsj");e.default=function(){return u.a.createElement(o.a,null,u.a.createElement(r.a,{title:"404: Not Found"}),u.a.createElement("h1",null,"Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist."))};var l="1097489062"}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-2aa192112b95c5c07fda.js.map
